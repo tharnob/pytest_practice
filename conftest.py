0514ddcf624acf74367d12c9ef3b7bfd71b01b97
Tafsir Haque Arnob <tharnob@gmail.com>
2023-10-14 10:55:12 +0600

    Parametrizing 2

--- conftest.py
+++ conftest.py
@@ -1,11 +1,28 @@
 import pytest
 
 
-@pytest.fixture(scope="function", autouse=False)
-def setUp():
-    print("Launch Browser")
+@pytest.fixture(scope="session", autouse=True)
+def setUp(browser):
+
+    if browser == "chrome":
+        print("Launch Chrome")
+
+    elif browser == "FireFox":
+        print("Launch FireFox")
+
+    else:
+        print("Provide valid browser")
+
     print("Login")
     print("Browse products")
     yield
     print("Logoff")
     print("Close browser")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+
+@pytest.fixture(scope="session", autouse=True)
+def browser(request):
+    return request.config.getoption("--browser")
